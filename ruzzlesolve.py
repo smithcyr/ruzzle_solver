9fd82ab4ac80c8fb7cc74f38f74a6d1ab3df6179
smithcyr <smithcyr@grinnell.edu>
2013-01-13 01:37:53 -0600

    Ver 1.1
    100% increase in performance
    addWord function can quickly add a word to both a text file in your
    dictionary folder and the currently loaded dictionary.

--- ruzzlesolve.py
+++ ruzzlesolve.py
@@ -1,4 +1,3 @@
-import re
 from time import time
 MAXLENGTH = 10
 MINLENGTH = 2
@@ -29,11 +28,13 @@ def ruzzlesolve(board_str,board_modifiers):
 
 	# given a letter returns the integer point value ruzzle has for that letter
 	def letterevaluator(char):
+		if (char in ['x']):
+			return 8
 		if (char in ['k']):
 			return 5
 		if (char in ['b','c','f','h','p','w','y']):
 			return 4
-		if (char in ['m','g']):
+		if (char in ['m','g','v']):
 			return 3
 		if (char in ['d','u']):
 			return 2
@@ -42,29 +43,27 @@ def ruzzlesolve(board_str,board_modifiers):
 	# attach the letter tuples to the board with the correct modifiers coordinates
 	# and the values of each respective letter
 	for i, L in enumerate(board_str):
-		# print L," ",
-		# if (not (i+1)%4) :
-		# 	print ""
 		board[i/4].append((L,board_modifiers[i],letterevaluator(L),(i%4,i/4)))
 
 	# recursive solver. 
 	# started on all 16 die it goes through all possible letter paths and adds words that 
 	# corresponds to the loaded dictionary to the words array
-	def solve(currentword, x, y):
+	def solve(wurd,currentword, x, y):
 		if (x > 3 or x < 0 or y > 3 or y < 0 or board[y][x] in currentword or len(currentword) > MAXLENGTH):
 			return
+		V = currentword+(board[y][x],)
+		W = wurd + board[y][x][0]
 		if (len(currentword) >= MINLENGTH):
-			V = currentword+(board[y][x],)
-			if ("".join(map(lambda v: v[0], V)) in dictionary):
+			if (W in dictionary):
 				words.append(V)
-		solve(currentword+(board[y][x],),x+1,y+1)
-		solve(currentword+(board[y][x],),x+1,y)
-		solve(currentword+(board[y][x],),x+1,y-1)
-		solve(currentword+(board[y][x],),x,y+1)
-		solve(currentword+(board[y][x],),x,y-1)
-		solve(currentword+(board[y][x],),x-1,y+1)
-		solve(currentword+(board[y][x],),x-1,y)
-		solve(currentword+(board[y][x],),x-1,y-1)
+		solve(W,V,x+1,y+1)
+		solve(W,V,x+1,y)
+		solve(W,V,x+1,y-1)
+		solve(W,V,x,y+1)
+		solve(W,V,x,y-1)
+		solve(W,V,x-1,y+1)
+		solve(W,V,x-1,y)
+		solve(W,V,x-1,y-1)
 
 	# Calculates the score of a word
 	# Used after all vetted words have been found
@@ -100,7 +99,7 @@ def ruzzlesolve(board_str,board_modifiers):
 	for x in range(4):
 		for y in range(4):
 			print "X",
-			solve(tuple(),x,y)
+			solve("",tuple(),x,y)
 		print ""
 	print "done"
 
@@ -126,3 +125,17 @@ def ruzzlesolve(board_str,board_modifiers):
 	for word in wordlist:
 		print "%3d :: %s" % (word[1],word[0])
 	print "Solved in",time()-starttime,"seconds"
+
+# our dictionary is not completely the same as ruzzle's
+# I added this function to make it easy to add words to the dictionary
+def addWord (word):
+	if (word in dictionary):
+		print "Already included"
+		return
+	if (not word[0] in "abcdefghijklmnopqrstuvwxyz"):
+		print "invalid word"
+		return
+	else:
+		dictionary.add(word)
+		newwords = open(DICTPATH + "extra.txt",'a')
+		newwords.write(word+'\n')
